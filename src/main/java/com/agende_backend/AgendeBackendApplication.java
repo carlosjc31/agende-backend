d3dd1dd5e894f08b56dfb8d97538c0fb95ec8737
carlosjc31 <carlosjc31@hotmail.com>
2026-01-02 13:23:53 -0300

    create first commit in the project

---
+++ src/main/java/com/agende_backend/AgendeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.agende_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendeBackendApplication.class, args);
+	}
+
+}
